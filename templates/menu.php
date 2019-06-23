39f636c15e37b994ec1150a1b2b4c423ec70e6c6
Allan M. Fernandes <allan.fernandes@webjump.com.br>
2019-06-22 22:13:10 -0300

    menu

---
+++ templates/menu.php
@@ -0,0 +1,13 @@
+<nav id="nav">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="cadastrar.php">Cadastre-se</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="entrar.php">Entre</a>
+    </li>
+  </ul>
+</nav>
